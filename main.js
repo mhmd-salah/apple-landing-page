fa9bc3cd34e6c7de54d00fc2bf1a37e1cd920a2f
mhmd <ms.mhmdsalah@gmail.com>
2024-01-31 22:14:14 +0200

    200

---
+++ main.js
@@ -0,0 +1,12 @@
+// hello dear;
+let img = document.querySelector( ".img" );
+let container = document.querySelector(".container")
+
+function phons (phon) {
+  img.src = phon.src
+}
+  
+function colors (color) {
+  container.style.background  = color
+}
+// end 
